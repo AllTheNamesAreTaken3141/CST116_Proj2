b472a1344a1c50de605eda79439d6a05fe89e6e9
EdwardFine <edward.fine@oit.edu>
2022-11-09 16:43:00 -0800

    Replacing code after fixing

--- Source/CST116-Proj2/main.cpp
+++ Source/CST116-Proj2/main.cpp
@@ -44,9 +44,122 @@ string whitespaceInput()
 
 #pragma endregion
 
+using std::cout;
+using std::cin;
+using std::endl;
+void displayMenu();
+int readInput();
+int getData();
+void processMenuChoice(int, int);
+void isPosNeg(int);
+void isOddEven(int);
+void findNumDigits(int);
+void findDigitAtPosition(int);
+void displayAdditionTable(int);
+void displayMultiplicationTable(int);
 int main()
 {
-	colorPrint("Hello World!", 9);
+	int menuSelection = 1;
+	while (menuSelection != 7)
+	{
+		displayMenu();
+		menuSelection = readInput();
+		if (menuSelection == 7)
+			break;
+		int data = getData();
+		processMenuChoice(menuSelection, data);
+	}
+}
+void displayMenu()
+{
+	cout << "What do you want to do with your number to test? Please enter number." << endl;
+	cout << "1. Check if the number is positive or negative." << endl;
+	cout << "2. Check if the number is even or odd." << endl;
+	cout << "3. Find the number of digits in the number." << endl;
+	cout << "4. Find the digit at a certain position." << endl;
+	cout << "5. Display an addition table from 0-10. " << endl;
+	cout << "6. Display a multiplication table from 0-10." << endl;
+	cout << "7. End program." << endl << endl;
+	return;
+}
+int readInput()
+{
+	int choice = 1;
+	cout << "Enter your choice: ";
+	cin >> choice;
+	return choice;
+}
+int getData() {
+	int data = 0;
+	cout << "What number do you want to test? ";
+	cin >> data;
+	cout << endl;
+	return data;
+}
+void processMenuChoice(int menuChoice, int data) {
+	switch (menuChoice) {
+	case 1:
+		isPosNeg(data);
+		break;
+	case 2:
+		isOddEven(data);
+		break;
+	case 3:
+		findNumDigits(data);
+		break;
+	case 4:
+		findDigitAtPosition(data);
+		break;
+	case 5:
+		displayAdditionTable(data);
+		break;
+	case 6:
+		displayMultiplicationTable(data);
+		break;
+	case 7:
+		break;
+	}
+}
+void isPosNeg(int val1)
+{
+	if (val1 < 0) {
+		cout << "Your number is negative." << endl;
+	}
+	else if (val1 > 0) {
+		cout << "Your number is positive." << endl;
+	}
+	else {
+		cout << "Your number is 0, therefore not positive or negative." << endl;
+	}
+	cout << endl << "----------------" << endl << endl;
+	return;
+}
+void isOddEven(int val1)
+{
+	int temp = abs(val1) % 2;
+	if (val1 == 0) {
+		cout << "Your number is 0, therefore it is not negative nor positive." << endl;
+		cout << endl << "----------------" << endl << endl;
+	}
+	else if (temp == 1) {
+		cout << "Your number is odd." << endl;
+	}
+	else if (temp == 0) {
+		cout << "Your number is even." << endl;
+	}
+	cout << endl << "----------------" << endl << endl;
+	return;
+}
+void findNumDigits(int val1)
+{
+	// ...
+}
+void findDigitAtPosition(int val1)
+{
+	// ...
+}
+void displayAdditionTable(int val1) {
 
-	return 0;
+}void displayMultiplicationTable(int val1) {
+
 }
